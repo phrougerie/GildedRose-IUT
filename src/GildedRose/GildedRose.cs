55f7f302bec7a8b81bad33b9d93b4a71e52ae762
Philippe Rougerie <philipperougerie14@gmail.com>
2022-01-23 17:00:34 +0100

    Sortie des if.

--- src/GildedRose/GildedRose.cs
+++ src/GildedRose/GildedRose.cs
@@ -19,17 +19,7 @@ namespace GildedRose
 
         private void UpdateQuality(Item item)
         {
-            if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
-            {
-                if (item.Quality > 0)
-                {
-                    if (item.Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        item.Quality = item.Quality - 1;
-                    }
-                }
-            }
-            else
+            if (item.Name == "Aged Brie" )
             {
                 if (item.Quality < 50)
                 {
@@ -54,37 +44,67 @@ namespace GildedRose
                         }
                     }
                 }
+
+            }
+            else
+            {
+
+                if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                {                    
+                }
+                else
+                {
+                    if (item.Quality > 0)
+                    {
+                        if (item.Name == "Sulfuras, Hand of Ragnaros")
+                        {
+                        }
+                        else
+                        {
+                            item.Quality = item.Quality - 1;
+                        }
+                    }
+                }
+                                
             }
 
-            if (item.Name != "Sulfuras, Hand of Ragnaros")
+            if (item.Name == "Sulfuras, Hand of Ragnaros")
+            {
+            }
+            else
             {
                 item.SellIn = item.SellIn - 1;
             }
 
             if (item.SellIn < 0)
             {
-                if (item.Name != "Aged Brie")
-                {
-                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (item.Quality > 0)
-                        {
-                            if (item.Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                item.Quality = item.Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        item.Quality = item.Quality - item.Quality;
-                    }
-                }
-                else
+                if (item.Name == "Aged Brie")
                 {
                     if (item.Quality < 50)
                     {
                         item.Quality = item.Quality + 1;
+                    }
+                    
+                }
+                else
+                {
+                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        item.Quality = item.Quality - item.Quality;
+                    }
+                    else
+                    {
+                        if (item.Quality > 0)
+                        {
+                            if (item.Name == "Sulfuras, Hand of Ragnaros")
+                            {
+                            }
+                            else
+                            {
+                                item.Quality = item.Quality - 1;
+
+                            }
+                        }
                     }
                 }
             }
